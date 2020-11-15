07cb45e30cf1a446490cc608b5c56c47dd9fb3c2
arnaud cormier <arno.cormier@gmail.com>
2020-11-15 15:13:42 +0100

    generate tracks migration

---
+++ priv/repo/migrations/20201115140144_create_tracks.exs
@@ -0,0 +1,14 @@
+defmodule Musicdb.Repo.Migrations.CreateTracks do
+  use Ecto.Migration
+
+  def change do
+    create table("tracks") do
+      add :title, :string, size: 200
+      add :duration, :integer, null: :false
+      add :index, :integer
+      add :number_of_plays, :integer
+
+      timestamps()
+    end
+  end
+end
