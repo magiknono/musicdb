40b3f987e82f2a1e0b2ef2a93d7519ea3d7f330a
arnaud cormier <arno.cormier@gmail.com>
2020-11-16 13:50:54 +0100

    add cmd to run seed

--- priv/repo/seeds/track.exs
+++ priv/repo/seeds/track.exs
@@ -1,4 +1,5 @@
 alias Musicdb.Repo
+# mix run [PATH_TO_FILE].exs
 
 # Insert 1 record in Track
 # Repo.insert(%Track{
@@ -9,5 +10,7 @@ alias Musicdb.Repo
 # }
 # )
 
+# Insert 1 record in track with Schema and changeset
+# Musicdb.Tracks.create_track(%{title: "hello"})
 
 
