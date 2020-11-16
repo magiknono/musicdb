d2948661323f94e070e46d1a8807d9ed20b88873
arnaud cormier <arno.cormier@gmail.com>
2020-11-16 14:04:04 +0100

    generate albums migration and update tracks with album_id

---
+++ priv/repo/migrations/20201116125733_create_albums.exs
@@ -0,0 +1,7 @@
+defmodule Musicdb.Repo.Migrations.CreateAlbums do
+  use Ecto.Migration
+
+  def change do
+
+  end
+end
---
+++ priv/repo/migrations/20201116130122_add_album_id_to_tracks.exs
@@ -0,0 +1,7 @@
+defmodule Musicdb.Repo.Migrations.AddAlbumIdToTracks do
+  use Ecto.Migration
+
+  def change do
+
+  end
+end
